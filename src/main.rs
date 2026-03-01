148cac11e68fa2ae8b3f25790b923bc48fac7e2b
kdnakt <a.kid.1985@gmail.com>
2026-03-01 14:19:35 +0900

    parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} script", args[0]);
+        return;
+    }
+
+    // TODO
+    println!("Running script: {}", args[1]);
 }
