c8954c6a24c3ead036b69a31c91262416f1c101c
kdnakt <a.kid.1985@gmail.com>
2026-03-09 00:53:56 +0900

    vm execute()

--- src/vm.rs
+++ src/vm.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::{parse::ParseProto, value::Value};
+use crate::{bytecode::ByteCode, parse::ParseProto, value::Value};
 
 pub struct ExeState {
     globals: HashMap<String, Value>,
@@ -18,6 +18,34 @@ impl ExeState {
     }
 
     pub fn execute(&mut self, proto: &ParseProto) {
+        for code in proto.byte_codes.iter() {
+            match *code {
+                ByteCode::GetGlobal(dst, name) => {
+                    let name = &proto.constants[name as usize];
+                    if let Value::String(key) = name {
+                        let v = self.globals.get(key).unwrap_or(&Value::Nil).clone();
+                        self.set_stack(dst, v);
+                    } else {
+                        panic!("invalid global key: {name:?}");
+                    }
+                }
+                ByteCode::LoadConst(dst, idx) => {
+                    let v = proto.constants[idx as usize].clone();
+                    self.set_stack(dst, v);
+                }
+                ByteCode::Call(func, _) => {
+                    let func = &self.stack[func as usize];
+                    if let Value::Function(f) = func {
+                        f(self);
+                    } else {
+                        panic!("invalid function: {func:?}");
+                    }
+                }
+            }
+        }
+    }
+
+    fn set_stack(&mut self, idx: u8, val: Value) {
         todo!()
     }
 }
