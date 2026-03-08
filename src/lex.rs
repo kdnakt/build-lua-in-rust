a860cd35e26d4a343024e4b6167a08680fd0f0c7
kdnakt <a.kid.1985@gmail.com>
2026-03-08 12:37:23 +0900

    lexer

--- src/lex.rs
+++ src/lex.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Token {
     Name(String),
     String(String),
@@ -18,16 +18,44 @@ pub struct Lex {
 impl Lex {
     pub fn new(input: File) -> Self {
         let content = std::io::read_to_string(input).unwrap();
-        Self {
+        Lex::_new(content)
+    }
+
+    fn _new(content: String) -> Self {
+        let mut lexer = Self {
             content,
             pos: 0,
             read_pos: 0,
             ch: '\0',
-        }
+        };
+        lexer.read_char();
+        lexer
     }
 
     pub fn next(&mut self) -> Token {
-        todo!()
+        self.skip_whitespace();
+        match self.ch {
+            '\0' => Token::Eos,
+            '"' => {
+                let start = self.pos + 1;
+                while self.peek_char() != '"' && self.peek_char() != '\0' {
+                    self.read_char();
+                }
+                self.read_char();
+                let s = self.content[start..self.pos].to_string();
+                self.read_char(); // skip closing quote
+                Token::String(s)
+            }
+            _ => {
+                let start = self.pos;
+                while !self.peek_char().is_whitespace() && self.peek_char() != '\0' {
+                    self.read_char();
+                }
+                self.read_char();
+                let name = self.content[start..self.pos].to_string();
+                Token::Name(name)
+            }
+        }
     }
 
     fn read_char(&mut self) {
@@ -50,5 +78,19 @@ impl Lex {
         {
             self.read_char();
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello_world() {
+        let input = r#"print "Hello, World!""#.to_string();
+        let mut lex = Lex::_new(input);
+        assert_eq!(lex.next(), Token::Name("print".to_string()));
+        assert_eq!(lex.next(), Token::String("Hello, World!".to_string()));
+        assert_eq!(lex.next(), Token::Eos);
     }
 }
