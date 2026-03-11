4f17be9f1e54204c3b04a35d183b0349cdd2f69a
kdnakt <a.kid.1985@gmail.com>
2026-03-12 00:40:21 +0900

    add values

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -5,6 +5,9 @@ use crate::vm::ExeState;
 #[derive(Clone)]
 pub enum Value {
     Nil,
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
     String(String),
     Function(fn(&mut ExeState) -> i32),
 }
@@ -13,6 +16,9 @@ impl Debug for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Nil => write!(f, "Nil"),
+            Self::Boolean(b) => write!(f, "{b}"),
+            Self::Integer(i) => write!(f, "{i}"),
+            Self::Float(fl) => write!(f, "{fl:?}"),
             Self::String(s) => write!(f, "{s}"),
             Self::Function(_) => write!(f, "function"),
         }
